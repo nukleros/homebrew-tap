ba0f358d5ada8945cf081e56ab040d2757d87fdb
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-08-31 14:47:15 -0400

    fix: updated checksums for operator-builder 0.7.0
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- Formula/operator-builder.rb
+++ Formula/operator-builder.rb
@@ -11,7 +11,7 @@ class OperatorBuilder < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/nukleros/operator-builder/releases/download/v0.7.0/operator-builder_v0.7.0_Darwin_x86_64.tar.gz"
-      sha256 "1b94920b763d3b4d51730f562ba38b1c335294eba786fec966db18e0c8759ffd"
+      sha256 "14793d01be079d8a86d3f3d73f1a168b2f529fc7f43bd33fb985bf14fdfba749"
 
       def install
         bin.install "operator-builder"
@@ -22,7 +22,7 @@ class OperatorBuilder < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/nukleros/operator-builder/releases/download/v0.7.0/operator-builder_v0.7.0_Darwin_arm64.tar.gz"
-      sha256 "565835dce89c201a998f16bae67b850a07944881b10a7bccb64ba6c8a089a48e"
+      sha256 "90190e8f262b849798af0a47a6828779f86079a990c396c5194c903047c42667"
 
       def install
         bin.install "operator-builder"
@@ -36,7 +36,7 @@ class OperatorBuilder < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/nukleros/operator-builder/releases/download/v0.7.0/operator-builder_v0.7.0_Linux_x86_64.tar.gz"
-      sha256 "d8208f05a7f672fdc5b96158eaa8ff29e6c5a71bb35a3eb123f3583d7ef717ed"
+      sha256 "89aa7e6ff04832deeaef1ebf0a7419b9fa54218c9e4cf2c2422c6ae06e4b8fc6"
 
       def install
         bin.install "operator-builder"
@@ -47,7 +47,7 @@ class OperatorBuilder < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/nukleros/operator-builder/releases/download/v0.7.0/operator-builder_v0.7.0_Linux_arm64.tar.gz"
-      sha256 "74e57193d6c99da23ef0a95c0da014de739b6d1f85d2dce2156299c11a779498"
+      sha256 "d0a9b810ed06cd3eb91bd52bc8f3fc75c04fe6689ad2aeb9507ca7df472013ed"
 
       def install
         bin.install "operator-builder"
